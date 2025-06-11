7ffb9de28541386710100752f113b36d2c9bb82d
lukas <lukszx07x@gmail.com>
2025-06-11 11:49:45 -0300

    5 passos

---
+++ backend/logicaProgramacao/02_lacos.py
@@ -0,0 +1,21 @@
+
+i = 10
+lista =  [10, 15, 22, 33, 42, 55, 60, 73, 88, 91, 100]
+for i in lista:
+    print("contando:", i)
+    i+=1 
+
+
+
+# while 
+valor = 1 
+soma = 0 
+  
+while valor != "1":
+    valor = int (input("digite o valor: "))   
+    soma+=valor 
+    print("Valor total da soma: ", soma)
+
+
+
+
---
+++ backend/logicaProgramacao/03_funcao.py
@@ -0,0 +1,17 @@
+def ola(nome):
+    return "Ola, " + nome
+print (ola("lukas"))
+
+
+def media(a, b):
+    return (a + b) / 2 
+print (media(20, 30))
+
+
+def par(numero):
+    if (numero % 2 == 0): 
+       return print ("é par")
+    else: 
+          return print ("é impar")
+
+print (par(9))
