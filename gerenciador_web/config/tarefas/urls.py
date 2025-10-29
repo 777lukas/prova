0a7945feecbe6261e636409236f43e5ebd6a8da4
lukas <lukszx07x@gmail.com>
2025-10-29 11:31:29 -0300

    atv6

--- gerenciador_web/config/tarefas/urls.py
+++ gerenciador_web/config/tarefas/urls.py
@@ -4,3 +4,19 @@ from . import views #importa nossas views
 urlpatterns = [
     path('', views.listar_tarefas, name ='listar_tarefas') # seria /tarefas/
 ]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.listar_tarefas, name='lista_tarefas'),#seria/tarefa/
+    path('<int:tarefa_id>/', views.detalhe_tarefa, name='detalhe_tarefa'),
+
+    #adicionar tarefas
+    path('adicionar/', views.adicionar_tarefa,name = 'adicionar_tarefa'),
+
+    #adicionar tarefa
+    path('<int:tarefa_id>/alterar/', views.alterar_tarefa, name='alterar_tarefa'),
+
+#excluir tarefa
+path('<int:tarefa_id>/excluir/', views.excluir_tarefa, name='excluir_tarefa')
+    ]
--- gerenciador_web/config/tarefas/views.py
+++ gerenciador_web/config/tarefas/views.py
@@ -1,23 +1,81 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
-
+from projetos.models import Projeto
 from .models import Tarefa
-# from django.http import HttpResponse
-
-# def listar_tarefas(request):
-#     tarefas_salvas = Tarefa.objects.all()
-#     print(tarefas_salvas)
-#     return HttpResponse("View 'listar_tarefas' executada! Confira no terminal")
-
 
 def listar_tarefas(request):
     # 1. a busca no banco de dados continua a mesma
     tarefas_salvas = Tarefa.objects.all()
-    #2. criamos um "dicionario do contexto" para enviar os dados ao template.
-    # A chave 'minhas_tarefas' sera a variavel que usarmos no HTML.
+
+    # 2. criamos um "dicionario do contexto" para enviar os dados ao template.
+    # A chave 'minhas_tarefas' será a variável que usaremos no html.
     contexto = {
-        'minhas_tarefas' : tarefas_salvas
+        'minhas_tarefas': tarefas_salvas 
     }
-    #3. redenrizarmos o template, passando a requisição e o contexto com os dados.
+
+    # 3. renderizamos o tamplate, passando a requisição e o contexto com os dados.
     return render(request, 'tarefas/lista.html', contexto)
+
+def detalhe_tarefa(request, tarefa_id):
+    #Busca uma tarefa pelo id
+    #Se não encontrar retorna um erro 404
+    tarefa= get_object_or_404 (Tarefa, pk=tarefa_id)
+
+    return render(request, 'tarefas/detalhe.html', {'tarefa':tarefa})
+
+def adicionar_tarefa(request):
+    projetos = Projeto.objects.all()
+    if request.method == 'POST':
+        titulo = request.POST.get('titulo')
+        descricao = request.POST.get('descricao')
+        projeto_id = request.POST.get('projeto') #pega o id do projeto selecionado
+
+        projeto_selecionado= Projeto.objects.get(pk= projeto_id) 
+
+        Tarefa.objects.create(titulo=titulo, descricao=descricao, projeto=projeto_selecionado)
+        
+        return redirect('listar_tarefas')
+    return render (request, 'tarefas/form_tarefa.html', {'projetos': projetos})
+
+#métodos HTTP
+#POST: envia dados para o servidor
+#GET: buscar dados no servidor
+#PUT: atualizar recursos existetes
+#DELETE: remove recursos selecionados
+
+def alterar_tarefa(request, tarefa_id):
+    tarefa = get_object_or_404(Tarefa, pk=tarefa_id)
+    projetos = Projeto.objects.all()
+    if request.method == 'POST':
+        titulo = request.POST.get('titulo')
+        descricao = request.POST.get('descricao')
+        projeto_id = request.POST.get('projeto') #pega o id do projeto selecionado
+        projeto_selecionado= Projeto.objects.get(pk= projeto_id) 
+        concluida = request.POST.get('concluida') == 'on'
+
+        projeto_selecionado= get_object_or_404(Projeto, pk= projeto_id)
+
+
+
+      
+        tarefa.titulo = titulo
+        tarefa.descricao = descricao
+        tarefa.concluida = concluida
+        tarefa.projeto = projeto_selecionado
+        tarefa.save()
+        
+        return redirect('lista_tarefas')
+
+    context = {
+        'tarefa': tarefa,
+         'projeto': tarefa,
+         }
+    return render(request, 'tarefas/form_tarefas.html', context)
+
+def excluir_tarefa(request, tarefa_id):
+    tarefa = get_object_or_404(Tarefa, pk=tarefa_id)
+    if request.method == 'POST':
+        tarefa.delete()
+        return redirect('lista_tarefa')
+    return render(request, 'tarefas/confirmar_exclusao.html', {'tarefa': tarefa})
+
